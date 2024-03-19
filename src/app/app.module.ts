de477f8c49000399c5e397daa66354cbb57d0fe1
Kgotso Bapela <kgotso.bapela@rain.co.za>
2024-03-19 17:28:35 +0200

    Completed recipe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { ReceipeItemComponent } from './recipes/recipe-list/receipe-item/receipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
@@ -18,9 +18,9 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipesComponent,
     RecipeDetailComponent,
     RecipeListComponent,
-    ReceipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    RecipeItemComponent,
   ],
   imports: [
     BrowserModule
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "This a simple test recipe", "https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/ef/db/64/efdb6476-58e7-c308-ef12-8ca02b8639c6/source/512x512bb.jpg")
+  ];
 }
